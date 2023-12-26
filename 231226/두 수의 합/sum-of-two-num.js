ff27f09d65711d1922c39af88d49f95d7df88df2
Dong Joon Oh <96936113+DonOhhhh@users.noreply.github.com>
2023-12-26 18:51:42 +0900

    Title: 두 수의 합, Time: 3719ms, Memory: 160MB, Status: Memory Limit Exceed - CodeTree

--- 231226/두 수의 합/sum-of-two-num.js
+++ 231226/두 수의 합/sum-of-two-num.js
@@ -2,10 +2,16 @@ const [[n, k], nums] = `${require("fs").readFileSync(0)}`.trim().split`
 `.map(row => row.trim().split` `.map(Number))
 
 let answer = 0
+const HashMap = {}
 
 for (let i = 0; i < n; i++) {
     for (let j = i + 1; j < n; j++) {
-        if (nums[i] + nums[j] === k) answer++;
+        const [ij, ji] = [`${nums[i]}${nums[j]}`, `${nums[j]}${nums[i]}`]
+        if (!(HashMap[ij] || HashMap[ji])) {
+            HashMap[ij] = nums[i] + nums[j]
+            HashMap[ji] = nums[i] + nums[j]
+        }
+        if (HashMap[ij] === k) answer++
     }
 }
 
