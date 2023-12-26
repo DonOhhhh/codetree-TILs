b31fdcfe907d97e1c439e51711e4417322c80cbe
Dong Joon Oh <96936113+DonOhhhh@users.noreply.github.com>
2023-12-27 05:25:34 +0900

    Title: 두 수의 합, Time: 443ms, Memory: 33MB, Status: Passed - CodeTree

--- 231227/두 수의 합/sum-of-two-num.js
+++ 231227/두 수의 합/sum-of-two-num.js
@@ -1,17 +1,13 @@
 const [[n, k], nums] = `${require("fs").readFileSync(0)}`.trim().split`
 `.map(e => e.trim().split` `.map(Number))
 
-function Counter(arr) {
-    arr.forEach(e => this[e] = (this[e] || 0) + 1)
+const counted = {}
+let ans = 0
+
+for (const num of nums) {
+    const diff = k - num
+    ans += (counted[diff] || 0)
+    counted[num] = (counted[num] || 0) + 1
 }
 
-const counted = new Counter(nums)
-let ans = 0;
-
-for (let i = 0; i < n; i++) {
-    const diff = Math.abs(k - nums[i])
-    if (counted[diff]) ans += counted[diff] > 1 ? counted[diff] - 1 : counted[diff];
-}
-
-
-console.log(parseInt(ans / 2))
+console.log(ans)
