3d466e9eeb5f77aad561e8075d864bb5b244928e
Dong Joon Oh <96936113+DonOhhhh@users.noreply.github.com>
2024-01-03 10:57:42 +0900

    Title: 자주 등장한 top k 숫자, Time: 632ms, Memory: 45MB, Status: Passed - CodeTree

--- 240103/자주 등장한 top k 숫자/top-k-frequent-elements.js
+++ 240103/자주 등장한 top k 숫자/top-k-frequent-elements.js
@@ -5,18 +5,7 @@ function Counter(arr) {
     arr.forEach(e => this[e] = (this[e] || 0) + 1)
 }
 
-const counted = new Counter(nums)
-const ans = Object.entries(counted)
-    .sort((a, b) => {
-        [a, b] = [a.map(Number), b.map(Number)]
-        if (a[1] < b[1]) {
-            return 1;
-        } else if (a[1] > b[1]) {
-            return -1;
-        } else {
-            return b[0] - a[0]
-        }
-    })
-    .filter((_, i) => i < kk)
+const counted = Object.entries(new Counter(nums))
+    .sort((a, b) => a[1] < b[1] ? 1 : +a[0] < +b[0] ? 1 : -1)
     .map(([k, _]) => k)
-console.log(...ans)
+console.log(...counted.slice(0, kk))
