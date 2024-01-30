d8a07fa3a888e497e6f8e59ad9652e88c45dd8ba
Dong Joon Oh <96936113+DonOhhhh@users.noreply.github.com>
2024-01-31 06:44:37 +0900

    Title: 자리 바꾸기 2, Time: 1554ms, Memory: 71MB, Status: Wrong Answer - CodeTree

--- 240131/자리 바꾸기 2/changing-seats-2.js
+++ 240131/자리 바꾸기 2/changing-seats-2.js
@@ -2,21 +2,20 @@ const [[n, k], ...inputs] = `${require("fs").readFileSync(0)}`.trim().split`
 `.map(row => row.trim().split` `.map(Number))
 
 const seats_info = Array(n).fill().map((_, i) => i + 1);
-const cnt = Array(n+1).fill().map((_,i) => new Set([i]))
+const cnt = Array(n + 1).fill().map((_, i) => new Set([i]))
 
 let minute = 0;
 
-while (minute <= 3 * k) {
-    const [a, b] = inputs[minute % k];
-    cnt[seats_info[a-1]].add(b);
-    cnt[seats_info[b-1]].add(a);
+for (let _ = 0; _ <= 3 * k; _++) {
+    const [a, b] = inputs[_ % k];
+    cnt[seats_info[a - 1]].add(b);
+    cnt[seats_info[b - 1]].add(a);
     [seats_info[a - 1], seats_info[b - 1]] = [seats_info[b - 1], seats_info[a - 1]];
-    minute++;
     // console.log({ seats_info })
 }
 
-// console.log({ seats_cnt })
+// console.log({ cnt })
 
-for(let i = 1; i <= n ; i++) {
+for (let i = 1; i <= n; i++) {
     console.log(cnt[i].size)
 }
