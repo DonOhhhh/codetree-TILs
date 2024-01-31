091c368721425dae95d0c34ff3f8fd183a1f314e
Dong Joon Oh <96936113+DonOhhhh@users.noreply.github.com>
2024-01-31 21:39:16 +0900

    Title: 자리 바꾸기 2, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240131/자리 바꾸기 2/changing-seats-2.js
+++ 240131/자리 바꾸기 2/changing-seats-2.js
@@ -1,14 +1,14 @@
 const [[n, k], ...inputs] = `${require("fs").readFileSync(0)}`.trim().split`
 `.map(row => row.trim().split` `.map(Number))
 
-const seats_info = Array(n).fill().map((_, i) => i + 1);
+const seats_info = Array(n + 1).fill().map((_, i) => i);
 const cnt = Array(n + 1).fill().map((_, i) => new Set([i]))
 
 for (let _ = 0; _ < 3; _++) {
     for (const [a, b] of inputs) {
-        [seats_info[a - 1], seats_info[b - 1]] = [seats_info[b - 1], seats_info[a - 1]];
-        cnt[seats_info[a - 1]].add(b);
-        cnt[seats_info[b - 1]].add(a);
+        [seats_info[a], seats_info[b]] = [seats_info[b], seats_info[a]];
+        cnt[seats_info[a]].add(b);
+        cnt[seats_info[b]].add(a);
     }
 }
 
